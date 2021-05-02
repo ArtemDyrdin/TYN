ad3809b8af93bbda834bb97c9b25b8f65c69515e
ArtemDyrdin <74466573+ArtemDyrdin@users.noreply.github.com>
2021-05-02 19:56:23 +0200

    преобразовал dataurl в файл изображения

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,41 @@
 import asyncio
 import websockets
+import numpy as np
+import urllib.request as ur
+from cv2 import cv2
+from PIL import Image
 
 connected = set()
 
-async def server(websocket, path):
-    # Register.
-    connected.add(websocket)
-    try:
-        async for message in websocket:
-            print(message)
+print('server started \n')
 
-            for conn in connected:
-                if conn == websocket:
-                    await conn.send(f'Got a new MSG FOR YOU: {message}')
-    finally: 
-        # Unregister.
-        connected.remove(websocket)
-    
+async def server(websocket, path):
+  # Register.
+  connected.add(websocket)
+  print('websocket opened \n')
+  try:
+    async for message in websocket:
+
+      # extract the contents of the URL:
+      try:
+        url_response = ur.urlopen(message)
+
+        # convert it into a numpy array
+        img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
+
+        # decode the image (готово для использования в библиотеке cv2)
+        img = cv2.imdecode(img_array, -1)
+        print(img)
+        # cv2.imshow('URL Image', img)
+        # cv2.waitKey()
+        print('done \n')
+      except:
+        print('error with url decoding')
+
+  except:
+    # Unregister.
+    print('ERROR!!!! \n websocket closed \n')
+    connected.remove(websocket)
 
 start_server = websockets.serve(server, "localhost", 5000)
 
